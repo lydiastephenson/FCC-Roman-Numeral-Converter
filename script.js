5c08c4877e9c43691020f41453958e6a8e23fc0a
Lydia <lydiastephenson@proton.me>
2023-06-03 15:27:48 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function convertToRoman(num) {
+    const ROMAN = { 'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1 }
+
+    let result = [];
+    
+    for (let k in ROMAN) {
+        while (ROMAN[k] <= num) {
+            num -= ROMAN[k];
+            result.push(k);
+        }
+    }
+
+    return result.join('');
+}
